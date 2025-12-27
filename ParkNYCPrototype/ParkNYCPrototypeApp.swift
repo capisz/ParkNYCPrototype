73ef377a70eb1dbb1441e3f05d7a5bb39750638d
capisz <ccapizzuto@yahoo.com>
2025-12-27 13:25:34 -0500

    Initial Commit

---
+++ ParkNYCPrototype/ParkNYCPrototypeApp.swift
@@ -0,0 +1,32 @@
+//
+//  ParkNYCPrototypeApp.swift
+//  ParkNYCPrototype
+//
+//  Created by NYCDOE on 12/27/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ParkNYCPrototypeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ParkNYCPrototypeTests/ParkNYCPrototypeTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParkNYCPrototypeTests.swift
+//  ParkNYCPrototypeTests
+//
+//  Created by NYCDOE on 12/27/25.
+//
+
+import Testing
+
+struct ParkNYCPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
